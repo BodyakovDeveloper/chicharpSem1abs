25596910903d323edffc83fed15c70564894777f
BodyakovDeveloper <59065454+BodyakovDeveloper@users.noreply.github.com>
2020-09-26 01:34:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Laba_1_Sprinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float S, V, T;
+
+            Console.Write("Введите сколько времени бежал спринтер(в минутах): ");
+
+            T = float.Parse(Console.ReadLine());
+
+            Console.Write("Введите с какой скоростью бежал спринтер(м/с): ");
+
+            V = float.Parse(Console.ReadLine());
+
+            S = V * T*60;
+
+            Console.WriteLine("Расстояние которое пробежал спринтер:  " + S + " метров");
+
+            Console.ReadLine();
+        }
+    }
+}
