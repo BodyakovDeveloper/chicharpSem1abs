d94d1ebe06bc8c61e7c3d07dc8d79d967c69d870
BodyakovDeveloper <59065454+BodyakovDeveloper@users.noreply.github.com>
2020-09-26 01:40:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Laba_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double u = 0, S = 1;
+            int start, end;
+
+            Console.WriteLine("Введите начальное значение: ");
+            do
+            {
+                start = Convert.ToInt32(Console.ReadLine());
+                if (start < 0)
+                {
+                    Console.WriteLine("Введите другое значение: ");
+                }
+            } while (start < 0);
+
+            Console.WriteLine("Введите значения за которое нельзя перейти: ");
+            do
+            {
+                end = Convert.ToInt32(Console.ReadLine());
+                if (end < start)
+                {
+                    Console.WriteLine("Введите другое значение: ");
+                }
+            } while (end < start);
+
+            for (int i = start; i <= end; i++)
+            {
+                u = (i * i - Math.Pow(-1, i * i + 1) * i) / (Math.Pow(i, 2) + 2);
+                S *= u;
+                Console.WriteLine("u" + i + "=" + u);
+                u = 0;
+            }
+            Console.WriteLine("Произведение ряда = " + S);
+
+            Console.ReadLine();
+        }
+    }
+}
