d619ba384f879f413ed560a655009e8bdf9e086c
BodyakovDeveloper <59065454+BodyakovDeveloper@users.noreply.github.com>
2020-09-28 12:37:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Laba_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int Num, n1, n2, n3, sum;
+            Console.Write("enter a three-digit number:");
+			Num = Int32.Parse(Console.ReadLine());
+			n1 = Num / 100;
+			n2 = Num / 10 - n1 * 10;
+			n3 = Num % 10;
+			sum = n1 + n2 + n3;
+            Console.WriteLine("Sum of numbers: " + sum);
+            if ( (sum % 2) == 0)
+            {
+                Console.WriteLine("Сумма чисел трехзначного числа парная");
+            }
+            else 
+			{
+				Console.WriteLine("Сумма чисел трехзначного числа непарная");
+			}
+			Console.ReadLine();
+		}
+	}
+}
+
