9db5b519214a45912770fd31cf30e62b28151b14
BodyakovDeveloper <59065454+BodyakovDeveloper@users.noreply.github.com>
2020-12-26 17:47:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+       
+    class Program
+    {
+        static void AddLast(ref int[] arr, int value)
+        {
+            int[] newArray = new int[arr.Length + 1];
+
+            newArray[arr.Length] = value;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArray[i] = arr[i];
+            }
+            arr = newArray;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 25, 38, 4, 100, 200, 45, 13, 100, 4 };
+
+            Console.WriteLine("Array before adding the last element:");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}; ");
+            }
+            AddLast(ref arr, 1000);
+
+            Console.WriteLine("\n\nArray after adding the last element:");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}; ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
